8b6796b25cead1993b9b56d66b6a72a797c07c6d
Huy <93058128+Acorn27@users.noreply.github.com>
2022-09-27 10:01:54 -0500

    Add files via upload

---
+++ selection_sort.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+
+void sortArray(int unsortedArray[], int size);
+
+int main()
+{
+
+    int unsortedArray[] = {2, 8, 1, 3, 6, 7, 5, 10, 99, -100, -12, -145};
+
+    int size = sizeof(unsortedArray) / sizeof(int);
+
+
+    sortArray(unsortedArray, size);
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ",unsortedArray[i]);
+    }
+
+
+}
+
+// recursive selection sort
+void sortArray(int unsortedArray[], int size)
+{
+
+    if (size == 1) 
+    {
+        printf("Array is sorted\n");
+    }
+    else
+    {   
+
+        int biggest_index = 0;
+        int change = 1;
+
+        // search for biggest index in slide
+        for (int i = 1; i <= size - 1; i++)
+        {   
+            // < for descending order
+            // > for ascending order
+            if (unsortedArray[i] > unsortedArray[biggest_index])
+            {
+                biggest_index = i;
+            }
+        }
+
+        // swap
+        int temp = unsortedArray[size - 1];
+        unsortedArray[size - 1] = unsortedArray[biggest_index];
+        unsortedArray[biggest_index] = temp;
+        
+        // recursive call
+        sortArray(unsortedArray, size - 1);
+    }
+
+
+
+}
